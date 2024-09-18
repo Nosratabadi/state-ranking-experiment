53ac02dfcfadd26bee379dc0ee3d0460edaf8abe
Nosratabadi <115089072+Nosratabadi@users.noreply.github.com>
2024-09-17 22:54:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ let isSecondRound = false;
 let delegatedToAI = false;
 let correctAnswers = 0;
 
-const GOOGLE_SHEET_URL = 'YOUR_NEW_GOOGLE_APPS_SCRIPT_URL_HERE';
+const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzMSglP2u10JClDCATBcs8BXoI8Znf96PJlszpS99avhxuzNxNOrorcmtfZMsUpbvwL/exec';
 
 function loadStimulus() {
     if (currentTrial < trialsPerRound) {
